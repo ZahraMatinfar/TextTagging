20356b5953d7ce10cb6f6f05530feed2917df3cc
unknown <z.matinfa77@gmail.com>
2024-11-02 23:37:13 +0330

    Add media

--- TextTagging/urls.py
+++ TextTagging/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,4 @@ urlpatterns = [
 
     path("api/", include("routers.urls")),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
